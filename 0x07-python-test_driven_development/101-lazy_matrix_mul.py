44bd323c59d043ce84ea54f3b057a3e5a0397e0c
lonjie254 <105755934+lonjie254@users.noreply.github.com>
2022-09-20 22:29:39 +0300

    Create 101-lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Matrix multiplication function using NumPy."""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplication of two matrices.
+    Args:
+        m_a The first matrix.
+        m_b  The second matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
